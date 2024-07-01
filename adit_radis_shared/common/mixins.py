6a8d119f09977b2480c3f9ef5405b41541e0e51a
Sander Niels Hummerich <64867257+hummerichsander@users.noreply.github.com>
2024-07-01 10:00:57 +0200

    add RelatedPaginationMixin

--- adit_radis_shared/common/mixins.py
+++ adit_radis_shared/common/mixins.py
@@ -147,3 +147,56 @@ class PageSizeSelectMixin:
         context["page_size_select"] = PageSizeSelectForm(self.request.GET, self.page_sizes)
 
         return context
+
+
+class RelatedPaginationMixinProtocol(ViewProtocol, Protocol):
+    request: AuthenticatedHttpRequest
+    object_list: QuerySet
+    paginate_by: int
+
+    def get_object(self) -> Any:
+        ...
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        ...
+
+    def get_related_queryset(self) -> QuerySet:
+        ...
+
+
+class RelatedPaginationMixin:
+    """This mixin provides pagination for a related queryset. This makes it possible to
+    paginate a related queryset in a DetailView. The related queryset is obtained by
+    the `get_related_queryset()` method that must be implemented by the subclass.
+    If used in combination with `RelatedFilterMixin`, the `RelatedPaginationMixin` must be
+    inherited first."""
+    def get_related_queryset(self: RelatedPaginationMixinProtocol) -> QuerySet:
+        raise NotImplementedError("You must implement this method")
+
+    def get_context_data(self: RelatedPaginationMixinProtocol, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if "object_list" in context:
+            queryset = context["object_list"]
+        else:
+            queryset = self.get_related_queryset()
+
+        paginator = Paginator(queryset, self.paginate_by)
+        page = self.request.GET.get("page")
+
+        if page is None:
+            page = 1
+
+        try:
+            paginated_queryset = paginator.page(page)
+        except PageNotAnInteger:
+            paginated_queryset = paginator.page(1)
+        except EmptyPage:
+            paginated_queryset = paginator.page(paginator.num_pages)
+
+        context["object_list"] = paginated_queryset
+        context["paginator"] = paginator
+        context["is_paginated"] = paginated_queryset.has_other_pages()
+        context["page_obj"] = paginated_queryset
+
+        return context
