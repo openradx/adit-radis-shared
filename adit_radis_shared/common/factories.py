4b9a715bc00e115b58dc7e6fdb34fe0ecf4e8581
Kai Schlamp <kai.schlamp@gmail.com>
2024-08-31 18:31:56 +0000

    Upgrade packages

--- adit_radis_shared/common/factories.py
+++ adit_radis_shared/common/factories.py
@@ -1,14 +1,10 @@
-from typing import Generic, TypeVar
-
 import factory
 from faker import Faker
 
 fake = Faker()
 
-T = TypeVar("T")
 
-
-class BaseDjangoModelFactory(Generic[T], factory.django.DjangoModelFactory):
+class BaseDjangoModelFactory[T](factory.django.DjangoModelFactory):
     @classmethod
     def create(cls, *args, **kwargs) -> T:
         return super().create(*args, **kwargs)
