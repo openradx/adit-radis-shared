649e8e62732e6df35381d14fb6c0552ebddb7673
Kai Schlamp <kai.schlamp@gmail.com>
2025-02-14 09:57:21 +0000

    Use typer directly as cli

--- adit_radis_shared/maintenance_commands/management/commands/generate_django_secret_key.py
+++
@@ -1,8 +0,0 @@
-from ..base.maintenance_command import MaintenanceCommand
-
-
-class Command(MaintenanceCommand):
-    """Generate a Django secret key"""
-
-    def handle(self):
-        print(self.generate_django_secret_key())
