461af1418c64834cfdb4d8d0ab958575625168ce
Kai Schlamp <kai.schlamp@gmail.com>
2025-10-07 15:55:14 +0200

    For cli use typer instead of argparse (#113)

--- cli.py
+++ cli.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python3
-# PYTHON_ARGCOMPLETE_OK
-import argparse
 import os
 import shutil
 import sys
 
+import typer
+
+from adit_radis_shared.cli import commands
 from adit_radis_shared.cli import helper as cli_helper
-from adit_radis_shared.cli import parsers
-from adit_radis_shared.cli.setup import setup_root_parser
+
+app = typer.Typer(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    no_args_is_help=True,
+)
+
+app.command()(commands.init_workspace)
+app.command()(commands.randomize_env_secrets)
+app.command()(commands.compose_build)
+app.command()(commands.compose_pull)
+app.command()(commands.compose_up)
+app.command()(commands.compose_down)
+app.command()(commands.stack_deploy)
+app.command()(commands.stack_rm)
+app.command()(commands.lint)
+app.command()(commands.format_code)
+app.command()(commands.test)
+app.command()(commands.shell)
+app.command()(commands.show_outdated)
+app.command()(commands.db_backup)
+app.command()(commands.db_restore)
+app.command()(commands.generate_auth_token)
+app.command()(commands.generate_secure_password)
+app.command()(commands.generate_django_secret_key)
+app.command()(commands.generate_certificate_chain)
+app.command()(commands.generate_certificate_files)
+app.command()(commands.upgrade_postgres_volume)
+app.command()(commands.try_github_actions)
 
 
-def copy_statics(**kwargs):
+@app.command()
+def copy_statics():
+    """Copy statics to Django's static folder"""
+
     helper = cli_helper.CommandHelper()
 
     print("Copying statics...")
@@ -40,38 +70,5 @@ def copy_statics(**kwargs):
     copy_file("node_modules/htmx.org/dist/ext/alpine-morph.js", "htmx-alpine-morph.js")
 
 
-def main():
-    root_parser = argparse.ArgumentParser()
-    subparsers = root_parser.add_subparsers(dest="command")
-
-    parsers.register_compose_build(subparsers)
-    parsers.register_compose_up(subparsers)
-    parsers.register_compose_down(subparsers)
-    parsers.register_compose_pull(subparsers)
-    parsers.register_db_backup(subparsers)
-    parsers.register_db_restore(subparsers)
-    parsers.register_format_code(subparsers)
-    parsers.register_generate_auth_token(subparsers)
-    parsers.register_generate_certificate_chain(subparsers)
-    parsers.register_generate_certificate_files(subparsers)
-    parsers.register_generate_django_secret_key(subparsers)
-    parsers.register_generate_secure_password(subparsers)
-    parsers.register_init_workspace(subparsers)
-    parsers.register_lint(subparsers)
-    parsers.register_randomize_env_secrets(subparsers)
-    parsers.register_shell(subparsers)
-    parsers.register_show_outdated(subparsers)
-    parsers.register_stack_deploy(subparsers)
-    parsers.register_stack_rm(subparsers)
-    parsers.register_test(subparsers)
-    parsers.register_try_github_actions(subparsers)
-    parsers.register_upgrade_postgres_volume(subparsers)
-
-    parser = subparsers.add_parser("copy-statics", help="Copy statics for the project")
-    parser.set_defaults(func=copy_statics)
-
-    setup_root_parser(root_parser)
-
-
 if __name__ == "__main__":
-    main()
+    app()
